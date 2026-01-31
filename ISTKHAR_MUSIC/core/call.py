cd5215f449adeb8071da4ed0071e03b32e961886
ISTKHAR-king <istkharalam225@gmail.com>
2026-01-31 21:47:55 +0530

    Update call.py

--- ISTKHAR_MUSIC/core/call.py
+++ ISTKHAR_MUSIC/core/call.py
@@ -17,20 +17,20 @@ from pytgcalls import PyTgCalls, exceptions, types
 from pytgcalls.pytgcalls_session import PyTgCallsSession
 
 import config
-from AloneMusic import LOGGER, YouTube, app
-from AloneMusic.misc import db
-from AloneMusic.utils.database import (add_active_chat, add_active_video_chat,
+from ISTKHAR_MUSIC import LOGGER, YouTube, app
+from ISTKHAR_MUSIC.misc import db
+from ISTKHAR_MUSIC.utils.database import (add_active_chat, add_active_video_chat,
                                        get_lang, get_loop, group_assistant,
                                        is_autoend, music_on,
                                        remove_active_chat,
                                        remove_active_video_chat, set_loop)
-from AloneMusic.utils.errors import capture_internal_err
-from AloneMusic.utils.exceptions import AssistantErr
-from AloneMusic.utils.formatters import (check_duration, seconds_to_min,
+from ISTKHAR_MUSIC.utils.errors import capture_internal_err
+from ISTKHAR_MUSIC.utils.exceptions import AssistantErr
+from ISTKHAR_MUSIC.utils.formatters import (check_duration, seconds_to_min,
                                          speed_converter)
-from AloneMusic.utils.inline.play import stream_markup
-from AloneMusic.utils.stream.autoclear import auto_clean
-from AloneMusic.utils.thumbnails import get_thumb
+from ISTKHAR_MUSIC.utils.inline.play import stream_markup
+from ISTKHAR_MUSIC.utils.stream.autoclear import auto_clean
+from ISTKHAR_MUSIC.utils.thumbnails import get_thumb
 from strings import get_string
 
 
@@ -601,4 +601,4 @@ class Call(PyTgCalls):
                         await self.stop_stream(update.chat_id)
 
 
-Alone = Call()
+ISTKHAR = Call()
