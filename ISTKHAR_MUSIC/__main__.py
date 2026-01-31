c33a36bc9c31c937070c27e731a8ce6c85a8af67
ISTKHAR-king <istkharalam225@gmail.com>
2026-01-31 22:12:51 +0530

    Update __main__.py

--- ISTKHAR_MUSIC/__main__.py
+++ ISTKHAR_MUSIC/__main__.py
@@ -1,61 +1,76 @@
 import asyncio
 import importlib
-from sys import argv
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
+from config import BANNED_USERS
 from ISTKHAR_MUSIC import LOGGER, app, userbot
-from ISTKHAR_MUSIC.core.call import noor
+from ISTKHAR_MUSIC.core.call import Call
 from ISTKHAR_MUSIC.misc import sudo
 from ISTKHAR_MUSIC.plugins import ALL_MODULES
 from ISTKHAR_MUSIC.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
+
+
+noor = Call()
 
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
+    if not any(
+        [
+            config.STRING1,
+            config.STRING2,
+            config.STRING3,
+            config.STRING4,
+            config.STRING5,
+        ]
     ):
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
-        exit()
+        LOGGER(__name__).error(
+            "Assistant client variables not defined, exiting..."
+        )
+        return
+
     await sudo()
+
     try:
-        users = await get_gbanned()
-        for user_id in users:
+        for user_id in await get_gbanned():
             BANNED_USERS.add(user_id)
-        users = await get_banned_users()
-        for user_id in users:
+        for user_id in await get_banned_users():
             BANNED_USERS.add(user_id)
-    except:
-        pass
+    except Exception as e:
+        LOGGER(__name__).warning(e)
+
     await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("ISTKHAR_MUSIC.plugins" + all_module)
+
+    for module in ALL_MODULES:
+        importlib.import_module(f"ISTKHAR_MUSIC.plugins{module}")
+
     LOGGER("ISTKHAR_MUSIC.plugins").info("Successfully Imported Modules...")
+
     await userbot.start()
     await noor.start()
+
     try:
-        await noor.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
+        await noor.stream_call(
+            "https://graph.org/file/e999c40cb700e7c684b75.mp4"
+        )
     except NoActiveGroupCall:
         LOGGER("ISTKHAR_MUSIC").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\nStopping Bot..."
         )
-        exit()
-    except:
+        return
+    except Exception:
         pass
+
     await noor.decorators()
-    LOGGER("ISTKHAR_MUSIC").info(
-        "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @THUNDERDEVS ᴊᴏɪɴ @THUNDERDEVS , @ISTKHAR_BOT ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
-    )
+
+    LOGGER("ISTKHAR_MUSIC").info("Istkhar Music Bot Started Successfully")
+
     await idle()
+
     await app.stop()
     await userbot.stop()
-    LOGGER("ISTKHAR_MUSIC").info("Stopping istkhar Music Bot...")
+    LOGGER("ISTKHAR_MUSIC").info("Stopping Istkhar Music Bot...")
 
 
 if __name__ == "__main__":
